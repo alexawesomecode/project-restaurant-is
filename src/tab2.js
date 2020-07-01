0b47c96740454945013e31a773eb5b40b1000806
alexawesomecode <alexawesomecode@gmail.com>
2020-07-01 20:21:29 +0000

    structure fix

---
+++ src/tab2.js
@@ -0,0 +1,18 @@
+const tabSecond = (tab, content, card) => {
+  card.classList.add('move');
+
+  setTimeout(() => {
+    card.innerHTML = `
+
+wanna know paradise without needing to travel to thailand?
+wanna feel the bangkok vibes, khao san road noise ?
+miss chiang mai and want to taste again some khao soi?
+visit us!
+
+`;
+    content.style.backgroundImage = `url('../imgs/${tab}.jpg')`;
+    card.classList.remove('move');
+  }, 800);
+};
+
+export default tabSecond;
